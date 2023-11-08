09bf5bf7e3c6b16bfff31449341e0568954af6eb
Xavier Leprêtre <xavier.lepretre@gmail.com>
2023-11-08 18:36:09 +0400

    Add module files.

---
+++ module/autocli.go
@@ -0,0 +1,13 @@
+package module
+
+import (
+	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+)
+
+// AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
+func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
+	return &autocliv1.ModuleOptions{
+		Query: nil,
+		Tx:    nil,
+	}
+}
---
+++ module/module.go
@@ -0,0 +1,116 @@
+package module
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"cosmossdk.io/core/appmodule"
+	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/alice/checkers"
+	"github.com/alice/checkers/keeper"
+)
+
+var (
+	_ module.AppModuleBasic = AppModule{}
+	_ module.HasGenesis     = AppModule{}
+	_ appmodule.AppModule   = AppModule{}
+)
+
+// ConsensusVersion defines the current module consensus version.
+const ConsensusVersion = 1
+
+type AppModule struct {
+	cdc    codec.Codec
+	keeper keeper.Keeper
+}
+
+// NewAppModule creates a new AppModule object
+func NewAppModule(cdc codec.Codec, keeper keeper.Keeper) AppModule {
+	return AppModule{
+		cdc:    cdc,
+		keeper: keeper,
+	}
+}
+
+func NewAppModuleBasic(m AppModule) module.AppModuleBasic {
+	return module.CoreAppModuleBasicAdaptor(m.Name(), m)
+}
+
+// Name returns the checkers module's name.
+func (AppModule) Name() string { return checkers.ModuleName }
+
+// RegisterLegacyAminoCodec registers the checkers module's types on the LegacyAmino codec.
+// New modules do not need to support Amino.
+func (AppModule) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {}
+
+// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the checkers module.
+func (AppModule) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *gwruntime.ServeMux) {
+	// if err := checkers.RegisterQueryHandlerClient(context.Background(), mux, checkers.NewQueryClient(clientCtx)); err != nil {
+	// 	panic(err)
+	// }
+}
+
+// RegisterInterfaces registers interfaces and implementations of the checkers module.
+func (AppModule) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	checkers.RegisterInterfaces(registry)
+}
+
+// ConsensusVersion implements AppModule/ConsensusVersion.
+func (AppModule) ConsensusVersion() uint64 { return ConsensusVersion }
+
+// RegisterServices registers a gRPC query service to respond to the module-specific gRPC queries.
+func (am AppModule) RegisterServices(cfg module.Configurator) {
+	// Register servers
+	// checkers.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
+	// checkers.RegisterQueryServer(cfg.QueryServer(), keeper.NewQueryServerImpl(am.keeper))
+
+	// Register in place module state migration migrations
+	// m := keeper.NewMigrator(am.keeper)
+	// if err := cfg.RegisterMigration(checkers.ModuleName, 1, m.Migrate1to2); err != nil {
+	// 	panic(fmt.Sprintf("failed to migrate x/%s from version 1 to 2: %v", checkers.ModuleName, err))
+	// }
+}
+
+// DefaultGenesis returns default genesis state as raw bytes for the module.
+func (AppModule) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	return cdc.MustMarshalJSON(checkers.NewGenesisState())
+}
+
+// ValidateGenesis performs genesis state validation for the circuit module.
+func (AppModule) ValidateGenesis(cdc codec.JSONCodec, _ client.TxEncodingConfig, bz json.RawMessage) error {
+	var data checkers.GenesisState
+	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
+		return fmt.Errorf("failed to unmarshal %s genesis state: %w", checkers.ModuleName, err)
+	}
+
+	return data.Validate()
+}
+
+// InitGenesis performs genesis initialization for the checkers module.
+// It returns no validator updates.
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) {
+	var genesisState checkers.GenesisState
+	cdc.MustUnmarshalJSON(data, &genesisState)
+
+	if err := am.keeper.InitGenesis(ctx, &genesisState); err != nil {
+		panic(fmt.Sprintf("failed to initialize %s genesis state: %v", checkers.ModuleName, err))
+	}
+}
+
+// ExportGenesis returns the exported genesis state as raw bytes for the circuit
+// module.
+func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+	gs, err := am.keeper.ExportGenesis(ctx)
+	if err != nil {
+		panic(fmt.Sprintf("failed to export %s genesis state: %v", checkers.ModuleName, err))
+	}
+
+	return cdc.MustMarshalJSON(gs)
+}
